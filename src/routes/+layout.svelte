7676359fc00e0b4cdcfe3c914c9c503faa149559
Italo Doliva <italocoltact@gmail.com>
2025-05-01 14:27:11 -0300

    GTAG added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,15 +6,26 @@
 	import ProjectView from '$lib/sections/ProjectView.svelte';
 	import Sidebar from '$lib/layouts/Sidebar.svelte';
 	import Stack from '$lib/layouts/Stack.svelte';
+	import { browser } from '$app/environment';
 	
 	let { children, data } = $props();
 
 	let HomeProjectContent = $derived(data.route === '/' && $currentProject.Content)
 
+	if (browser) {
+		window.dataLayer = window.dataLayer || []
+		window.gtag = function gtag() { 
+			window.dataLayer.push(arguments) 
+		}
+		window.gtag('js', new Date())
+		window.gtag('config', 'G-CJBPZ3FWP0')
+	}
+
 </script>
 
 <svelte:head>
 	<title>Italo&#8201;Doliva</title>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-CJBPZ3FWP0"></script>
 </svelte:head>
 
 <Sidebar sideOn='left' sideWidth="20%" gapK=0>
