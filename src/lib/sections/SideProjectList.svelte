2ef9f7cb87e98d991da124d2af3e24665478228f
Italo Doliva <italocoltact@gmail.com>
2025-05-28 17:19:29 -0300

    Fix: Sidebar is no longer with decreased height in mobile devices

--- src/lib/sections/SideProjectList.svelte
+++ src/lib/sections/SideProjectList.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <aside bind:clientWidth={width}>
-  <div class="fixed top-0 left-0 h-svh border-r border-r-gray-light overflow-auto" style:width={width + 'px'}>
+  <div class="fixed top-0 left-0 min-h-svh border-r border-r-gray-light overflow-auto" style:width={width + 'px'}>
 
     <Stack element="ul" borderWidth="1">
 
